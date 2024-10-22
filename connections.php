745a5d1c8501d513a09a74035b457fb40a07bdf9
Kzizah <silateizizah@gmail.com>
2024-10-22 21:40:03 +0300

    Added enviroment file

--- connections.php
+++ connections.php
@@ -1,17 +1,30 @@
 <?php
 
-//creating a variable begins with a dollar sign followed by the name of the variable
-//making a connection to the MySQL database
+// Load environment variables from .env file
+require 'vendor/autoload.php'; // Ensure this path is correct
 
-$databasehost = "mysql"; //name of the server
-$databaseuser = "root"; //username of the database
-$databasepassword = "6970"; //password
-$databasename = "parlour"; //database name of Beauty Parlour Management System
+use Dotenv\Dotenv;
 
-//create and check connection
-//test for errors
-if (!$conn = mysqli_connect($databasehost, $databaseuser, $databasepassword, $databasename)) {
+// Load the .env file
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// Creating a variable begins with a dollar sign followed by the name of the variable
+// Making a connection to the MySQL database
+
+$databasehost = "mysql"; // name of the server
+$databaseuser = $_ENV['MYSQL_USER']; // username of the database
+$databasepassword = $_ENV['MYSQL_PASSWORD']; // password
+$databasename = $_ENV['MYSQL_DATABASE']; // database name of Beauty Parlour Management System
+
+// Create and check connection
+// Test for errors
+$conn = mysqli_connect($databasehost, $databaseuser, $databasepassword, $databasename);
+
+if (!$conn) {
     echo "<script>alert('Connection failed!');</script>";
-    die("Failed to connect!");
-} 
+    die("Failed to connect: " . mysqli_connect_error());
+}
+// Close the connection (optional, you can keep it open if needed later)
+// mysqli_close($conn);
 ?>
--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -6,6 +6,11 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Attribute' => $vendorDir . '/symfony/polyfill-php80/Resources/stubs/Attribute.php',
     'Composer\\InstalledVersions' => $vendorDir . '/composer/InstalledVersions.php',
     'PHPGangsta_GoogleAuthenticator' => $vendorDir . '/phpgangsta/googleauthenticator/PHPGangsta/GoogleAuthenticator.php',
+    'PhpToken' => $vendorDir . '/symfony/polyfill-php80/Resources/stubs/PhpToken.php',
+    'Stringable' => $vendorDir . '/symfony/polyfill-php80/Resources/stubs/Stringable.php',
+    'UnhandledMatchError' => $vendorDir . '/symfony/polyfill-php80/Resources/stubs/UnhandledMatchError.php',
+    'ValueError' => $vendorDir . '/symfony/polyfill-php80/Resources/stubs/ValueError.php',
 );
